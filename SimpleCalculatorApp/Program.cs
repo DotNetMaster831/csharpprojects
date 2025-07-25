6b0014f9d103bb155d80cd5764d50ed381c13a7a
Kandregula Durga Sai <kdurgas@apac.corpdir.net>
2025-07-25 20:35:19 +0530

    simple calculator

---
+++ SimpleCalculatorApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace SimpleCalculatorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter first number");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second number");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Choose Operation (+ , -, /, *, %)");
+
+                char userChoice = Convert.ToChar(Console.ReadLine());
+
+                int result = 0;
+
+                if (userChoice == '+')
+                {
+                    result = Add(num1,num2);
+                    Console.WriteLine($"Result : {result}");
+                }
+                else if (userChoice == '-')
+                {
+                    Console.WriteLine($"Result : {num1 - num2}");
+                }
+                else if (userChoice == '*')
+                {
+                    Console.WriteLine($"Result : {num1 * num2}");
+                }
+                else if (userChoice == '/')
+                {
+                    Console.WriteLine($"Result : {num1 / num2}");
+                }
+                else if (userChoice == '%')
+                {
+                    Console.WriteLine($"Result : {num1 % num2}");
+                }
+                else
+                {
+                    Console.WriteLine("Inavlid Operation");
+                }
+
+
+                Console.WriteLine();
+
+
+                Console.WriteLine("Do u want to continue? (y/n)");
+                string cond = Console.ReadLine();
+
+                if (cond.ToLower() != "y")
+                    break;
+
+
+                
+            }
+
+
+            
+            //Console.ReadKey();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1+num2;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ SimpleCalculatorApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SimpleCalculatorApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Daimler AG")]
+[assembly: AssemblyProduct("SimpleCalculatorApp")]
+[assembly: AssemblyCopyright("Copyright © Daimler AG 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5aba137f-2a37-4c42-ba66-caab882ea4da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
